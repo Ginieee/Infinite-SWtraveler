7f977e8b3e7ca305b8b301c8d2ed67f4980e3e39
MinchoGreenT <dldmstjq99@gmail.com>
2023-10-30 17:32:59 +0900

    여러가지함

--- basic.js
+++ basic.js
@@ -85,6 +85,186 @@ class App {
         this._scene.add(plane);
         plane.receiveShadow = true;
 
+        const startPoint = new THREE.Vector3(-1000, 0, 1300);
+        const aiPoint = new THREE.Vector3(-300, 0, -450);
+        const gachonPoint = new THREE.Vector3(200, 0, 600);
+
+        new THREE.GLTFLoader().load("data/subway.glb", (gltf) => {
+            const subway = gltf.scene;
+            this._scene.add(subway);
+
+            subway.traverse(child => {
+                if(child instanceof THREE.Mesh) {
+                    child.castShadow = true;
+                }
+            });
+
+            const box = (new THREE.Box3).setFromObject(subway);
+            subway.position.y = (box.max.y - box.min.y) / 2;
+
+            subway.scale.set(20, 20, 20);
+            subway.position.x = startPoint.x;
+            subway.position.z = startPoint.z + 100;
+
+            subway.rotation.y = Math.PI / 2;
+
+            const boxHelper = new THREE.BoxHelper(subway);
+            this._scene.add(boxHelper);
+            this._boxHelper_subway = boxHelper;
+            this._subway = subway;
+        });
+
+        new THREE.GLTFLoader().load("data/bus.glb", (gltf) => {
+            const bus = gltf.scene;
+            this._scene.add(bus);
+
+            bus.traverse(child => {
+                if(child instanceof THREE.Mesh) {
+                    child.castShadow = true;
+                }
+            });
+
+            const box = (new THREE.Box3).setFromObject(bus);
+            bus.position.y = (box.max.y - box.min.y) / 2;
+
+            bus.scale.set(30, 30, 30);
+            bus.position.x = startPoint.x + 100;
+            bus.position.z = startPoint.z - 800;
+
+            bus.rotation.y = Math.PI / 2;
+
+            const boxHelper = new THREE.BoxHelper(bus);
+            this._scene.add(boxHelper);
+            this._boxHelper_bus = boxHelper;
+            this._bus = bus;
+        });
+
+        new THREE.GLTFLoader().load("data/bus_stop.glb", (gltf) => {
+            const busStop = gltf.scene;
+            this._scene.add(busStop);
+
+            busStop.traverse(child => {
+                if(child instanceof THREE.Mesh) {
+                    child.castShadow = true;
+                }
+            });
+
+            const box = (new THREE.Box3).setFromObject(busStop);
+            busStop.position.y = (box.max.y - box.min.y) / 2;
+
+            // set bus stop size bigger
+            busStop.scale.set(10, 10, 10);
+            busStop.position.x = startPoint.x + 100;
+            busStop.position.z = startPoint.z - 900;
+
+            busStop.rotation.y = Math.PI;
+
+            const boxHelper = new THREE.BoxHelper(busStop);
+            this._scene.add(boxHelper);
+            this._boxHelper_busStop = boxHelper;
+            this._busStop = busStop;
+        });
+
+        new THREE.GLTFLoader().load("data/building_art.glb", (gltf) => {
+            const artBuilding = gltf.scene;
+            this._scene.add(artBuilding);
+
+            artBuilding.traverse(child => {
+                if(child instanceof THREE.Mesh) {
+                    child.castShadow = true;
+                }
+            });
+
+            const box = (new THREE.Box3).setFromObject(artBuilding);
+            artBuilding.position.y = (box.max.y - box.min.y) / 2;
+
+            artBuilding.scale.set(6, 6, 6);
+            artBuilding.position.x = startPoint.x - 100;
+            artBuilding.position.z = aiPoint.z + 300;
+
+            artBuilding.rotation.y = - Math.PI / 3;
+
+            const boxHelper = new THREE.BoxHelper(artBuilding);
+            this._scene.add(boxHelper);
+            this._boxHelper_artBuilding = boxHelper;
+            this._artBuilding = artBuilding;
+        });
+
+        new THREE.GLTFLoader().load("data/ai_building.glb", (gltf) => {
+            const aiBuilding = gltf.scene;
+            this._scene.add(aiBuilding);
+
+            aiBuilding.traverse(child => {
+                if(child instanceof THREE.Mesh) {
+                    child.castShadow = true;
+                }
+            });
+
+            const box = (new THREE.Box3).setFromObject(aiBuilding);
+            aiBuilding.position.y = 30;
+
+            aiBuilding.scale.set(0.3, 0.3, 0.3);
+            aiBuilding.position.x = aiPoint.x;
+            aiBuilding.position.z = aiPoint.z;
+
+            aiBuilding.rotation.y = Math.PI;
+
+            const boxHelper = new THREE.BoxHelper(aiBuilding);
+            this._scene.add(boxHelper);
+            this._boxHelper_aiBuilding = boxHelper;
+            this._aiBuilding = aiBuilding;
+        });
+
+        new THREE.GLTFLoader().load("data/ai_cube.glb", (gltf) => {
+            const aiCube = gltf.scene;
+            this._scene.add(aiCube);
+
+            aiCube.traverse(child => {
+                if(child instanceof THREE.Mesh) {
+                    child.castShadow = true;
+                }
+            });
+
+            const box = (new THREE.Box3).setFromObject(aiCube);
+
+            aiCube.scale.set(1, 1, 1);
+            aiCube.position.x = aiPoint.x + 30;
+            aiCube.position.z = aiPoint.z - 30;
+            aiCube.position.y = 90;
+
+            aiCube.rotation.y = Math.PI;
+
+            const boxHelper = new THREE.BoxHelper(aiCube);
+            this._scene.add(boxHelper);
+            this._boxHelper_aiCube = boxHelper;
+            this._aiCube = aiCube;
+        });
+
+        new THREE.GLTFLoader().load("data/building_gachon.glb", (gltf) => {
+            const gachonBuilding = gltf.scene;
+            this._scene.add(gachonBuilding);
+
+            gachonBuilding.traverse(child => {
+                if(child instanceof THREE.Mesh) {
+                    child.castShadow = true;
+                }
+            });
+
+            const box = (new THREE.Box3).setFromObject(gachonBuilding);
+            gachonBuilding.position.y = (box.max.y - box.min.y) / 2;
+
+            gachonBuilding.scale.set(20, 20, 20);
+            gachonBuilding.position.x = gachonPoint.x;
+            gachonBuilding.position.z = gachonPoint.z;
+
+            gachonBuilding.rotation.y = Math.PI / 6;
+
+            const boxHelper = new THREE.BoxHelper(gachonBuilding);
+            this._scene.add(boxHelper);
+            this._boxHelper_gachonBuilding = boxHelper;
+            this._gachonBuilding = gachonBuilding;
+        });
+
         new THREE.GLTFLoader().load("data/character.glb", (gltf) => {
             const model = gltf.scene;
             this._scene.add(model);
@@ -109,6 +289,10 @@ class App {
             this._currentAnimationAction = this._animationMap["Idle"];
             this._currentAnimationAction.play();
 
+            model.scale.set(0.3, 0.3, 0.3);
+            model.position.x = startPoint.x;
+            model.position.z = startPoint.z;
+
             const box = (new THREE.Box3).setFromObject(model);
             model.position.y = (box.max.y - box.min.y) / 2;
 
@@ -130,13 +314,13 @@ class App {
             5000
         );
 
-        camera.position.set(0, 100, 500);
+        camera.position.set(-1000, 50, 1500);
         this._camera = camera;
     }
 
     _addPointLight(x, y, z, helperColor) {
         const color = 0xffffff;
-        const intensity = 0.8;
+        const intensity = 0.2;
     
         const pointLight = new THREE.PointLight(color, intensity, 2000);
         pointLight.position.set(x, y, z);
@@ -265,8 +449,36 @@ class App {
                 this._model.position.y,
                 this._model.position.z,
             );            
+
+            // this._model 과 this._bus 의 거리가 가까워지면 버스가 출발하도록
+            if(this._model && this._bus) {
+                const distance = this._model.position.distanceTo(this._bus.position);
+                if(distance < 400) {
+                    this._busFlag = true;
+                }
+            }
             
         }
+
+        if(this._boxHelper_subway) {
+            this._boxHelper_subway.update();
+        }
+
+        // move subway to x axis direction with 1 speed
+        if(this._subway) {
+            const moveX = 0.4;
+            this._subway.position.x += moveX;
+        }
+
+        if(this._boxHelper_bus) {
+            this._boxHelper_bus.update();
+        }
+
+        if(this._bus && this._busFlag) {
+            const moveX = 20;
+            this._bus.position.x += moveX;
+        }
+
         this._previousTime = time;
     }
 
